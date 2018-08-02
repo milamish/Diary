df96f5750c3671ddff17088ebd5744f01713d1b6
milamish <milamish@live.com>
2018-08-03 00:08:44 +0300

    modifying tests

--- API/api_v1/tests/test_entries.py
+++ API/api_v1/tests/test_entries.py
@@ -12,21 +12,22 @@ class Test_Diary3(unittest.TestCase):
 		self.app = app.test_client()
 	
 	def test_modify_an_entry(self):
-		self.assertEqual(app.test_client().get('/api/v2/entries,').status_code,404)
+		self.assertEqual(app.test_client().get('/api/v2/entries',).status_code,200)
+		self.assertEqual(app.test_client().post('/api/v2/entries',).status_code, 200)
 
 	def test_delete_entry(self):
 		m= app.test_client()
-		response=(m.get('/api/v2/entries,').status_code,403)
+		response=(m.get('/api/v2/entries',).status_code,403)
 
 	def test_view_entries(self):
 		m=app.test_client()
-		response=(m.get('/api/v2/entries,').status_code,403)
+		response=(m.get('/api/v2/entries',).status_code,403)
 
 	def test_single_entries(self):
-		self.assertEqual(app.test_client().get('/api/v2/entry,').status_code,404)
+		self.assertEqual(app.test_client().get('/api/v2/entries',).status_code,200)
 
 	def test_add_entry(self):
-		self.assertEqual(app.test_client().get('/api/v2/entries,').status_code,404)
+		self.assertEqual(app.test_client().get('/api/v2/entries',).status_code,200)
 
 if __name__ =='__main__':
     unittest.main()
--- API/api_v1/tests/test_users.py
+++ API/api_v1/tests/test_users.py
@@ -17,10 +17,17 @@ class Test_Diary(unittest.TestCase):
 	def test_login(self):
 		m=app.test_client()
 		response=(m.get('/api/v2/auth/login',).status_code, 500)
+		sign_data=json.dumps({})
+
+	def test_signed_in(self):
+		sign_data=json.dumps({"username":"mish", "password":"Milamish"})
+		header={"content-type":"application/json"}
+		signedin=app.test_client().post('/api/v2/auth/login',data=sign_data, headers=header)
+		response=signedin
 
 	def test_register(self):
-		response=(app.test_client().get('/api/v2/auth/signUp,').status_code,500)
-		respone2=(app.test_client().get('/api/v2/auth/signUp,').status_code,409)
+		response=(app.test_client().get('/api/v2/auth/signUp',).status_code,500)
+		respone2=(app.test_client().get('/api/v2/auth/signUp',).status_code,409)
 
 
 
